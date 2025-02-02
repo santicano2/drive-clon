65819d7ff04b2be51c69f328b7c7770b8c131736
Santi <canosantiago404@gmail.com>
2025-02-02 10:59:28 -0300

    feat: db content renderizando en ui

--- src/app/file-row.tsx
+++ src/app/file-row.tsx
@@ -1,6 +1,7 @@
-import type { Folder, File } from "~/lib/mock-data";
 import { Folder as FolderIcon, FileIcon } from "lucide-react";
-export function FileRow(props: { file: File }) {
+import type { files, folders } from "~/server/db/schema";
+
+export function FileRow(props: { file: typeof files.$inferSelect }) {
   const { file } = props;
   return (
     <li
@@ -25,7 +26,7 @@ export function FileRow(props: { file: File }) {
   );
 }
 export function FolderRow(props: {
-  folder: Folder;
+  folder: typeof folders.$inferSelect;
   handleFolderClick: () => void;
 }) {
   const { folder, handleFolderClick } = props;
