c2d19e450c591d27b590ffdcb8b834c9e9f7e3d5
Santi <canosantiago404@gmail.com>
2025-02-04 19:27:02 -0300

    fix: lint

--- src/env.js
+++ src/env.js
@@ -26,7 +26,6 @@ export const env = createEnv({
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
     NEXT_PUBLIC_POSTHOG_KEY: z.string(),
-    NEXT_PUBLIC_POSTHOG_HOST: z.string(),
   },
 
   /**
@@ -43,7 +42,6 @@ export const env = createEnv({
     SINGLESTORE_DB_NAME: process.env.SINGLESTORE_DB_NAME,
 
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
-    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
