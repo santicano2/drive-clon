fcff9229163bb14ce1d7d4106a802348dee32002
Santi <canosantiago404@gmail.com>
2025-02-04 17:40:35 -0300

    feat: añadidas analiticas con posthog

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,6 +1,6 @@
 import { QUERIES } from "~/server/db/queries";
 
-import DriveContents from "../../drive-contents";
+import DriveContents from "./drive-contents";
 
 export default async function GoogleDriveClone(props: {
   params: Promise<{ folderId: string }>;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { PostHogProvider } from "./_providers/posthog-provider";
 
 export const metadata: Metadata = {
   title: "Drive Local",
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
-        <body>{children}</body>
+        <body>
+          <PostHogProvider>{children}</PostHogProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
