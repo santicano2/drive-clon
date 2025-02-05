42a6a9a589ea4aa0c8768e82a92647fa6555afb2
Santi <canosantiago404@gmail.com>
2025-02-05 09:47:47 -0300

    feat: añadido crear drive

--- src/app/(home)/drive/page.tsx
+++ src/app/(home)/drive/page.tsx
@@ -1,15 +1,39 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
-import { QUERIES } from "~/server/db/queries";
+
+import { MUTATIONS, QUERIES } from "~/server/db/queries";
+
+import { Button } from "~/components/ui/button";
 
 export default async function DrivePage() {
   const session = await auth();
+
   if (!session.userId) {
     return redirect("/sign-in");
   }
+
   const rootFolder = await QUERIES.getRootFolderForUser(session.userId);
+
   if (!rootFolder) {
-    return redirect("/drive/create-root-folder");
+    return (
+      <form
+        action={async () => {
+          "use server";
+          const session = await auth();
+
+          if (!session.userId) {
+            return redirect("/sign-in");
+          }
+
+          const rootFolderId = await MUTATIONS.onboardUser(session.userId);
+
+          return redirect(`/f/${rootFolderId}`);
+        }}
+      >
+        <Button>Create new Drive</Button>
+      </form>
+    );
   }
+
   return redirect(`/f/${rootFolder.id}`);
 }
