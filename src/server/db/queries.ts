2eaf3d577b4f0d87bf6b17da5e7c4b78f64b33bb
Santi <canosantiago404@gmail.com>
2025-02-05 09:36:30 -0300

    feat: añadido homepage, sign in y comienzo de onboarding

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,6 +1,6 @@
 import "server-only";
 
-import { eq } from "drizzle-orm";
+import { and, eq, isNull } from "drizzle-orm";
 
 import { db } from "~/server/db";
 import {
@@ -45,6 +45,15 @@ export const QUERIES = {
       .select()
       .from(foldersSchema)
       .where(eq(foldersSchema.id, folderId));
+    return folder[0];
+  },
+  getRootFolderForUser: async function (userId: string) {
+    const folder = await db
+      .select()
+      .from(foldersSchema)
+      .where(
+        and(eq(foldersSchema.ownerId, userId), isNull(foldersSchema.parent)),
+      );
     return folder[0];
   },
 };
